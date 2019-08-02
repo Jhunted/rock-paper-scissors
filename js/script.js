479f7f11e07f2b8d6b10761cebe4fc56dfa5ce9a
Joseph Hunt <joseph@Josephs-MacBook-Pro.local>
2019-08-02 12:31:00 -0500

    adds results

--- js/script.js
+++ js/script.js
@@ -3,14 +3,43 @@ const beepAudio = new Audio('http://soundbible.com/mp3/Robot_blip-Marianne_Gagno
 const shootAudio = new Audio('http://soundbible.com/mp3/shooting_star-Mike_Koenig-1132888100.mp3');
 
 /*----- app's state (variables) -----*/
-
+let scores, results, winner;
 
 /*----- cached element references -----*/
-
+const scoreEls = {
+    p: document.getElementById('p-score'),
+    t: document.getElementById('t-score'),
+    c: document.getElementById('c-score')
+};
 
 /*----- event listeners -----*/
-
+document.querySelector('main button')
+    .addEventListener('click', playRound);
 
 /*----- functions -----*/
+init();
 
-alert('ready!')
+function init() {
+    scores = {
+    p: 0,
+    t: 0,
+    c: 0
+};
+
+results = {
+    p: 'r',
+    c: 'r' 
+};
+winner = null;  // 'p', 't', 'c'
+render();
+}
+
+function render() { // render scores
+for (let score in scores) {
+    scoresEls[score].textContent = scores[score];
+    }
+}
+
+function playRound() {
+    console.log('clicked');
+}
